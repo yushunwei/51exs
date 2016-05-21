e100b15093c0d3ddb2aa460b43abae5a8d92478e
yushunwei <yushunwei005@hotmail.com>
2016-05-21 20:32:13 +0800

    yushunwei modify

--- app/js/controllers/appopen/openController.js
+++ app/js/controllers/appopen/openController.js
@@ -10,6 +10,10 @@ define(["../../view/appopen/openView","../../tool/ajaxTool","../../tool/Utils"],
         //绑定select change事件
         bindEvents();
     }
+
+    /**
+     * header设置
+     */
     function setHeader(){
         var param = {
             "success":function(d){
@@ -18,6 +22,10 @@ define(["../../view/appopen/openView","../../tool/ajaxTool","../../tool/Utils"],
         };
         ajax.load("user",param);
     }
+
+    /**
+     * 加载行业
+     */
     function loadHY(){
         var _code = arguments.length == 0 ? "-1" : arguments[0];
         var index = arguments.length == 0 ? 0 : arguments[1];
@@ -31,6 +39,10 @@ define(["../../view/appopen/openView","../../tool/ajaxTool","../../tool/Utils"],
         };
         ajax.load("openHY",param);
     }
+
+    /**
+     * 加载地域
+     */
     function loadQY(){
         var _code = arguments.length == 0 ? "-1" : arguments[0];
         var index = arguments.length == 0 ? 0 : arguments[1];
@@ -45,6 +57,10 @@ define(["../../view/appopen/openView","../../tool/ajaxTool","../../tool/Utils"],
         ajax.load("openQY", param);
 
     }
+
+    /**
+     * 事件绑定
+     */
     function bindEvents(){
         var code,index;
         //行业下拉框change事件
@@ -72,40 +88,53 @@ define(["../../view/appopen/openView","../../tool/ajaxTool","../../tool/Utils"],
         });
         //表格提交
         $(".page-open").find("#submitbutton").on("click",function(){
+
+            // 表单输入验证
             if(!$('#signupForm').valid()) return;
-            var code = $("#invitecode").val();
-            var companyname = $("#companyname").val();
-            var industrys = ($("#industrys1 select").find("option:selected").attr("code")+"-"+ $("#industrys2 select").find("option:selected").attr("code")+"-"+ $("#industrys3 select").find("option:selected").attr("code")).split("-");
-            var areas = ($("#areas1 select").find("option:selected").attr("code")+"-"+ $("#areas2 select").find("option:selected").attr("code")+"-"+ $("#areas3 select").find("option:selected").attr("code")).split("-");
-            var agreement = $(".agreement input[type=checkbox]").is(":checked");
-            if(code &&companyname &&industrys.length==3&&areas.length==3 && agreement ){
-                var _data = {
-                    "inviteCode" : code,
-                    "companyName" : companyname,
-                    "industrys" : industrys,
-                    "areas" : areas
-                };
-                var param = {
-                    "data":JSON.stringify(_data),
-                    "type":"post",
-                    contentType: 'application/json',
-                    "beforeSend" : function(){
-                        $("#loading").show();
-                    },
-                    "success":function(d){
-                        $("#loading").hide();
-                        if(d.status == 200){
-                            location.href= "/index.html";
-                        }else{
-                            alert("开通失败" + d.subMsg);
-                        }
+
+            // 用户输入参数
+            var data = {
+                inviteCode:$("#invitecode").val(),
+                companyName:$("#companyname").val(),
+                industrys:[
+                           $("#industrys1 select").find("option:selected").text(),
+                           $("#industrys2 select").find("option:selected").text(),
+                           $("#industrys3 select").find("option:selected").text()
+                ],
+                areas:[
+                       $("#areas1 select").find("option:selected").text(),
+                       $("#areas2 select").find("option:selected").text(),
+                       $("#areas3 select").find("option:selected").text()
+                ]
+            };
+
+            // 提交参数
+            var param = {
+                data:JSON.stringify(data),
+                type:"post",
+                contentType: 'application/json',
+                beforeSend: function(){
+                    layer.msg('处理中，请稍后', {icon: 16,time:-1,shade:[0.4,'#CCC']});
+                },
+                success:function(d){
+                    $("#loading").hide();
+                    if(d.status == 200){
+                        location.href= "/index.html";
+                    }else{
+                        layer.alert('开通失败！'+ d.subMsg, {icon: 2});
                     }
-                };
-                ajax.load("openapplication",param);
-                return false;
-            }
+                },
+                error:function(){
+                    layer.alert('开通失败！', {icon: 2});
+                }
+            };
+            ajax.load("openapplication",param);
         });
     }
+
+    /**
+     * 表单验证
+     */
     function validateForm(){
         $.validator.addMethod(
             "allInput",     //自定义规则方法名
@@ -125,10 +154,10 @@ define(["../../view/appopen/openView","../../tool/ajaxTool","../../tool/Utils"],
         $.validator.addMethod(
             "isCode",     //自定义规则方法名
             function(value , element , param){   //自定义规则体
-                var b =  /(?!^\d+$)(?!^[a-zA-Z]+$)[0-9a-zA-Z]{4,23}/;
+                var b =  /[0-9a-zA-Z]{8}/;
                 return b.test(value);
             },
-            ''   //提示信息
+            '请将信息填写完整'   //提示信息
         );
         $('#signupForm').validate({
             rules: {
@@ -152,8 +181,8 @@ define(["../../view/appopen/openView","../../tool/ajaxTool","../../tool/Utils"],
             messages: {
                 code: {
                     required: "请输入邀请码！",
-                    isCode: "邀请码必须是数字英文组合",
-                    remote: "您输入的邀请码有误，请重新输入！"
+                    isCode: "请输入正确的8位邀请码",
+                    remote: ""
                 },
                 name: {
                     required: "请输入企业名称",
--- app/js/view/appopen/openView.js
+++ app/js/view/appopen/openView.js
@@ -1,38 +1,46 @@
 define([], function () {
+  /**
+   * 渲染行业下拉框
+   * @param data 下拉框数据
+   * @param index 行业下拉索引
+   * @private
+   */
   function _renderHY(data,index) {
     var $selectBox = $('.page-open .open-hy');
     if(data) {
-      $selectBox.find("select").eq(index).empty();
-      $selectBox.find("select").eq(index).append("<option value='-1'>请选择</option>");
+      var nextSelect = $selectBox.find("select").eq(index);
+      nextSelect.empty().append("<option value='-1'>请选择</option>");
 
       $.each(data.data, function (i, v) {
-        $selectBox.find("select").eq(index).append("<option code='" + this.code + "'>" + this.name + "</option>");
+        nextSelect.append("<option code='" + this.code + "'>" + this.name + "</option>");
       });
-      $selectBox.find(".select-box").eq(index).show();
-      $selectBox.find(".select-box").filter(":gt(" + (index - 0) + ")").hide();
-      $selectBox.find(".select-box").filter(":gt(" + (index - 0) + ")").find("select").empty();
-      $selectBox.find(".select-box").filter(":gt(" + (index - 1) + ")").find("select").val("-1");
-      $selectBox.find(".select-box").filter(":gt(" + (index - 1) + ")").find("input").val("-1");
+
+      $selectBox.find(".select-box").eq(index).show().find("select").show();
+      $selectBox.find(".select-box").filter(":gt(" + (index - 0) + ")").find("select").empty().hide();
     }else{
-      $selectBox.find(".select-box").eq(index - 1).show();
       $selectBox.find(".select-box").filter(":gt(" + (index - 1) + ")").hide();
     }
   }
+
+  /**
+   * 渲染地域数据
+   * @param data 下拉框数据
+   * @param index 地域下拉索引
+   * @private
+   */
   function _renderQY(data,index) {
     var $selectBox = $('.page-open .open-qy');
     if(data) {
-      $selectBox.find("select").eq(index).empty();
-      $selectBox.find("select").eq(index).append("<option value='-1'>请选择</option>");
-      $.each(data.data,function(i,v){
-        $selectBox.find("select").eq(index).append("<option code='"+this.code+"'>"+this.name+"</option>");
+      var nextSelect = $selectBox.find("select").eq(index);
+      nextSelect.empty().append("<option value='-1'>请选择</option>");
+
+      $.each(data.data, function (i, v) {
+        nextSelect.append("<option code='" + this.code + "'>" + this.name + "</option>");
       });
-      $selectBox.find(".select-box").eq(index).show();
-      $selectBox.find(".select-box").filter(":gt("+(index-0)+")").hide();
-      $selectBox.find(".select-box").filter(":gt("+(index-0)+")").find("select").empty();
-      $selectBox.find(".select-box").filter(":gt("+(index-1)+")").find("select").val("-1");
-      $selectBox.find(".select-box").filter(":gt("+(index-1)+")").find("input").val("-1");
+
+      $selectBox.find(".select-box").eq(index).show().find("select").show();
+      $selectBox.find(".select-box").filter(":gt(" + (index - 0) + ")").find("select").empty().hide();
     }else{
-      $selectBox.find(".select-box").eq(index - 1).show();
       $selectBox.find(".select-box").filter(":gt(" + (index - 1) + ")").hide();
     }
   }
