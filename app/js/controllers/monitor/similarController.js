796d85ad5ae148956f7dd38132bcd802ed9cd1c3
shuyuqiong <shuyuqiong1986@163.com>
2016-05-25 11:09:18 +0800

    修改查询列表信息

--- app/js/controllers/monitor/similarController.js
+++ app/js/controllers/monitor/similarController.js
@@ -1,5 +1,5 @@
 
-define(["../../tool/ajaxTool","../../view/monitor/similarView","../../common/commonView"], function (ajax,view,commonView) {
+define(["../../tool/ajaxTool","../../view/monitor/similarView","common/commonController","../../common/commonView"], function (ajax,view,common,commonView) {
     var $tar = $("div.page-allplan");
     var result = [];
     var fullViewInit = {
@@ -12,6 +12,7 @@ define(["../../tool/ajaxTool","../../view/monitor/similarView","../../common/com
         getMonitorInfoList(0,20);
         //绑定事件
         bindEvent();
+        common.handleEmail();
     }
 
     function getMonitorInfoList(pageNum,pageSize){
@@ -20,31 +21,19 @@ define(["../../tool/ajaxTool","../../view/monitor/similarView","../../common/com
         pageData.keywords = $('.conditions-searchbox').find('input').val();
         var param ={"query" :pageData};
         param.success = function(d){
-            view.renderList(d);
-            result = d;
-            pagination(result,pageNum);
+            if(d.status == 200) {
+                view.renderList(d);
+                result = d;
+                pagination(result, pageNum);
+            }else{
+                layer.alert(d.subMsg, {icon: 2});
+            }
         };
         ajax.load("getMonitorInfoList",param);
     }
     function bindEvent(){
         var $dom = $(".page-monitorinfolist");
-        // 时间范围选择时清空日期选择框
-        $('.type-timeranges').find('a').click(function(){
-            $('.input-daterange').find('input').val('');
-            $('#customdays').removeClass('active');
-            $(this).parent().parent().find("li a.active").removeClass("active");
-            $(this).addClass("active");
-            getMonitorInfoList(0,fullViewInit.pageSize);
-        });
-        $dom.find('.conditions-searchbox').find('button').click(function(){
-            getMonitorInfoList(0,20);
-        });
 
-        $("#btnFind").click(function(){
-            $('#customdays').addClass('active');
-            $('.type-timeranges').find('a').removeClass('active');
-            getMonitorInfoList(0,fullViewInit.pageSize);
-        });
         //在批量取消预警页面上绑定事件，弹出对话框并绑定id
         $('.cancel-alldanger-btn').click(function(){
             //如果选中不为空，弹出确认对话框
@@ -62,19 +51,6 @@ define(["../../tool/ajaxTool","../../view/monitor/similarView","../../common/com
             }else{
                 $('.cancel-danger-none').modal('show');
             }
-        });
-        $dom.find(".table.table-bordered").on("click",".email-send a",function(){
-            var param = {
-                "success":function(d){
-                    view.renderAddEmail(d);
-                }
-            }
-            ajax.load("addEmail",param);
-        });
-        //add email
-        $("form").on("submit",function(){
-            $(this).parent().find(".add-email-ul").append('<li><label class="cy-checkbox"><input type="checkbox"><span></span></label>'+$(this).find("input[type=email]").val()+'</li>')
-            return false;
         });
     }
     //分页
