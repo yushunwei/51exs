eb022b7ce9a72e0d310630844309cb0be4ece3bd
yushunwei <yushunwei005@hotmail.com>
2016-05-30 16:13:36 +0800

    yushunei modify

--- app/js/common/commonController.js
+++ app/js/common/commonController.js
@@ -45,6 +45,10 @@ define(["../tool/ajaxTool","../tool/Utils"], function (ajax,utils) {
         '</div>'+
         '</div>'+
         '</div>';
+
+    // 返回顶部页面元素
+    var backToTop = '<p id="back-to-top"><a href="javascript:void(0)"><span></span>返回顶部</a></p>';
+
     function _init(page) {
         //设置底部信息
         setFooter();
@@ -68,6 +72,14 @@ define(["../tool/ajaxTool","../tool/Utils"], function (ajax,utils) {
         ifNew();
         //单独取消预警
         (page.name=="allplan"||page.name=="fullView"||page.name=="warnCenter"||page.name=="monitorinfolist")&&cancelWarnOne(page);
+
+        //返回顶部初始化
+        initBackToTop({
+            domId:"back-to-top",
+            showDistance:100,
+            fadeTime:300,
+            scrollTime:200
+        });
 
     }
     //取消列表单个预警
@@ -525,11 +537,40 @@ define(["../tool/ajaxTool","../tool/Utils"], function (ajax,utils) {
                     }
                 },
                 error :function(){
-                    
+
                 }
             };
             ajax.load("canaddnewuserplan",param);
         });
+    }
+
+    /**
+     * 返回顶部统一添加
+     * @param opt
+     */
+    function initBackToTop(opt){
+        try{
+            $("body").append(backToTop);
+            opt.showDistance = opt.showDistance || 100;
+            opt.fadeTime = opt.fadeTime || 300;
+            opt.scrollTime = opt.scrollTime || 200;
+
+            $(window).scroll(function(){
+                if($(window).scrollTop()>opt.showDistance){
+                    $("#"+opt.domId).fadeIn(opt.fadeTime);
+                }
+                else{
+                    $("#"+opt.domId).fadeOut(opt.fadeTime);
+                }
+            });
+            //绑定点击事件
+            $("#"+opt.domId).click(function(){
+                $('body,html').animate({scrollTop:0},opt.scrollTime);
+                return false;
+            });
+        }catch(e){
+
+        }
     }
     // 返回
     return {
